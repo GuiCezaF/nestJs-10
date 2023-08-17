50f71900c0995887d0a4187719f2fac5ace3726f
GuiCezaF <contato.guicezafe@gmail.com>
2023-08-17 13:36:33 -0300

    feat: add video resource

---
+++ src/videos/dto/create-video.dto.ts
@@ -0,0 +1,26 @@
+import { Type } from 'class-transformer';
+import {
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  Min,
+} from 'class-validator';
+
+export class CreateVideoDto {
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  descripton: string | null;
+
+  @Min(1)
+  @IsInt()
+  @IsNotEmpty()
+  @Type(() => Number)
+  category_id: number;
+}
